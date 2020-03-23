6e4d854a513cd761b4d107b261b8bcbed25da4b8
Alex <60114079+Sashimee@users.noreply.github.com>
2020-03-23 11:52:12 +0100

    rooms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'rooms',
+    loadChildren: () => import('./rooms/rooms.module').then( m => m.RoomsPageModule)
   }
 ];
 
